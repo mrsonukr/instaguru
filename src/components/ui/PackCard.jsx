6a691b7f31f8249c486d7adaf092aedb54414b5c
mrsonukr <MSSONUKR@GMAIL.COM>
2025-08-12 14:17:11 +0530

    updated

--- src/components/ui/PackCard.jsx
+++ src/components/ui/PackCard.jsx
@@ -58,7 +58,11 @@ const PackCard = ({ color = "red", title, description, price, link, packId }) =>
       <div
         className={`flex-shrink-0 text-white text-sm px-4 py-2 rounded-full flex items-center gap-1 ${variant.buttonBg} ${variant.buttonHover}`}
       >
+        <div>
+          <span className="line-through mr-1">₹{price*2}</span>
         ₹{price}
+
+        </div>
         <ArrowRightIcon className="w-4 h-4" />
       </div>
     </Link>
